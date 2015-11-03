5bddef7adb47689f13ddaa10ebb3513380b684fd
Abraham-7 <noria0540@hotmail.com>
2015-11-03 13:45:15 -0500

    Got font not button

--- core/src/com/mygdx/btnmenu/Menubtn.java
+++ core/src/com/mygdx/btnmenu/Menubtn.java
@@ -1,7 +1,9 @@
 package com.mygdx.btnmenu;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
@@ -15,53 +17,153 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public class Menubtn extends ApplicationAdapter {
-	Skin skin;
-	Stage stage;
-	TextButton newGameButton;
-	static BitmapFont font;
-	SpriteBatch batch;
-	BitmapFontCache creators,begin;
-	boolean bBegin = true;
-	//@Override
-	public void createBasicSkin() {
-		font = new BitmapFont(Gdx.files.internal("fonts/8-BIT_WONDER.fnt"));
+public class Menubtn extends Game implements InputProcessor {
+    Skin skin;
+    Stage stage;
+    TextButton newGameButton;
+    BitmapFontCache creators, begin;
+    boolean bBegin = true;
 
-		creators = new BitmapFontCache(font);
-		skin = new Skin();
-		skin.add("default", font);
+    @Override
+    public void create() {
+        BitmapFont font = new BitmapFont();
 
-		//Create a texture
-		Pixmap pixmap = new Pixmap(Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
-		pixmap.setColor(Color.WHITE);
-		pixmap.fill();
-		skin.add("background", new Texture(pixmap));
+        stage = new Stage();
+        creators = new BitmapFontCache(font);
+        skin = new Skin();
+        skin.add("default", font);
 
-		//Create a button style
-		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("background", Color.BLACK);
-		textButtonStyle.down = skin.newDrawable("background", Color.BLACK);
-		textButtonStyle.checked = skin.newDrawable("background", Color.BLACK);
-		textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-		textButtonStyle.font = skin.getFont("default");
-		skin.add("default", textButtonStyle);
+        //Create a texture
+        Pixmap pixmap = new Pixmap(Gdx.graphics.getWidth() / 4, Gdx.graphics.getHeight() / 10, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("background", new Texture(pixmap));
 
-		newGameButton = new TextButton("Begin", skin); // Use the initialized skin
-		newGameButton.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2);
-		newGameButton.addListener(new ClickListener() {
-			public void clicked(InputEvent e, float x, float y) {
-			}
-		});
-	}
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//stage.act();
-		//stage.draw();
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.BLACK);
+        textButtonStyle.down = skin.newDrawable("background", Color.WHITE);
+        textButtonStyle.checked = skin.newDrawable("background", Color.BLACK);
+        textButtonStyle.over = skin.newDrawable("background", Color.WHITE);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        newGameButton = new TextButton("Begin", skin); // Use the initialized skin
+        newGameButton.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2);
+        newGameButton.addListener(new ClickListener() {
+            public void clicked(InputEvent e, float x, float y) {
+                System.out.println("test")
+                ;
+            }
+        });
+        stage.addActor(newGameButton);
+        Gdx.input.setInputProcessor(this);
+    }
+
+    //@Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
 
 
-	}
+    }
+
+    /**
+     * Called when a key was pressed
+     *
+     * @param keycode one of the constants in {@link Input.Keys}
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    /**
+     * Called when a key was released
+     *
+     * @param keycode one of the constants in {@link Input.Keys}
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    /**
+     * Called when a key was typed
+     *
+     * @param character The character
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    /**
+     * Called when the screen was touched or a mouse button was pressed. The button parameter will be {@link Buttons#LEFT} on iOS.
+     *
+     * @param screenX The x coordinate, origin is in the upper left corner
+     * @param screenY The y coordinate, origin is in the upper left corner
+     * @param pointer the pointer for the event.
+     * @param button  the button
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    /**
+     * Called when a finger was lifted or a mouse button was released. The button parameter will be {@link Buttons#LEFT} on iOS.
+     *
+     * @param screenX
+     * @param screenY
+     * @param pointer the pointer for the event.
+     * @param button  the button   @return whether the input was processed
+     */
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    /**
+     * Called when a finger or the mouse was dragged.
+     *
+     * @param screenX
+     * @param screenY
+     * @param pointer the pointer for the event.  @return whether the input was processed
+     */
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    /**
+     * Called when the mouse was moved without any buttons being pressed. Will not be called on iOS.
+     *
+     * @param screenX
+     * @param screenY
+     * @return whether the input was processed
+     */
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    /**
+     * Called when the mouse wheel was scrolled. Will not be called on iOS.
+     *
+     * @param amount the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
+     * @return whether the input was processed.
+     */
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
